6b5550de327b9e9319380112cb93108a2a1a9f19
Shambhavi Chandrawat <shambhavichandrawat06@gmail.com>
2026-06-27 00:52:08 +0530

    Animation engine basic logic and understanding

--- src/core/animationEngine.js
+++ src/core/animationEngine.js
@@ -4,11 +4,25 @@ export const STEP_TYPES = {
     SORTED: "sorted"
 }; //for making our code safer and prevent future spelking mistakes
 
-export function playSteps(steps) {
-console.log(steps);
-console.log(steps[0]);
-console.log(steps[0].type);
-console.log(steps[0].first);
-console.log(steps[0].second);
+export async function playSteps(steps) {
+    for (let i = 0; i < steps.length; i++) {
+        const currentStep = steps[i];
+    if (currentStep.type === STEP_TYPES.COMPARE) {
+        console.log(`Comparing ${currentStep.first} and ${currentStep.second}`);
+    }
+    else if (currentStep.type === STEP_TYPES.SWAP) {
+        console.log(`Swapping ${currentStep.first} and ${currentStep.second}`);
+    }
+    else if (currentStep.type === STEP_TYPES.SORTED) {
+        console.log(`Bar ${currentStep.index} is sorted`);
+    }
+    await sleep (1000); //causes a 1 second wait after each step
+  }
+}
 
+//adding the sleep function
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
